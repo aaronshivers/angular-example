20050c66c9cc01d7e976a3d3c28f770960519c37
aaron shivers <aaorn@moonmail.org>
2019-11-12 10:38:09 -0600

    feature/api added error handling

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -11,8 +13,21 @@ export class ApiService {
   constructor(private httpClient: HttpClient) {
   }
 
-  public get() {
-    console.log("hello");
-    return this.httpClient.get(this.SERVER_URL);
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+
+    if (error.error instanceof ErrorEvent) {
+      // client side errors
+      errorMessage = `Error: ${ error.error.message }`;
+    } else {
+      // server side errors
+      errorMessage = `Error Code: ${ error.status }\nMessage: ${ error.message }`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
+  public sendGetRequest() {
+    return this.httpClient.get(this.SERVER_URL).pipe(catchError(this.handleError));
   }
 }
