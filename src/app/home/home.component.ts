1e7cc6548c07dd7e0f1f1c571f5a90a243ab73cb
Aaron <42848750+aaronshivers@users.noreply.github.com>
2019-11-12 16:38:37 +0000

    Revert "feature/api added api service"

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: [ './home.component.scss' ],
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  products = [];
 
-  constructor(private apiService: ApiService) {
-  }
+  constructor() { }
 
   ngOnInit() {
-    this.apiService.get().subscribe((data: any[]) => {
-      console.log(data);
-      this.products = data;
-    });
   }
 
 }
